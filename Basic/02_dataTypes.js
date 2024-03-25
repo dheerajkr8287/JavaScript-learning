5d487fb7e1780b9e1302a188c73662e38c9ebcca
Dheeraj kumar <119065267+dheerajkr8287@users.noreply.github.com>
2024-03-25 18:23:02 +0000

    datatype

---
+++ Basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"//treat all js code as newer version
+//alert(3+3)//we are using node js ,not browser
+let name="dheeraj"
+let age=18
+let isLoggedIn=false
+let state=null
+
+
+//number =>2 to power 53
+//bigInt
+// string=>""
+//boolean=> true/false
+//null =>standalone value   :The Null type has exactly one value, called null.
+// undefined=>:The Undefined type has exactly one value, called undefined. Any variable that has not been assigned a value has the value undefined.
+// symbol=>unique
+// object 
+
+// console.log(typeof "dheeraj")
+// console.log(typeof age);
+
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
+ 
+
