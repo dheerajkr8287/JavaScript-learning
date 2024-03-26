ca884db397e29dc183a4cba82db8d1ac676ee077
Dheeraj kumar <119065267+dheerajkr8287@users.noreply.github.com>
2024-03-26 10:16:22 +0000

    coversionOperation

---
+++ Basic/03_conversionOperation.js
@@ -0,0 +1,60 @@
+let score ="33abc";
+// console.log(typeof score);
+// console.log(typeof (score));
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"=>33
+// "33abc"=>NaN
+//true=>1; false=>0
+//null=>0
+//undefined=>NaN
+
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1=>true;0=>false
+// ""=>false
+// "dheeraj"=>true
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ****************Operations******************
+
+
+let value=8;
+let negValue=-value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(3%2);
+
+let str1="hello";
+let str2="dheeraj";
+
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+2+2); //122
+// console.log(1+2+"2"); //32
+
+// console.log(+true);//1
+// console.log(+"");//0
+
+
+
+
+
