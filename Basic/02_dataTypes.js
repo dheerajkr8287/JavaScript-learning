06dd705a5871ac290d5c14b20a69ae8072035920
Dheeraj kumar <119065267+dheerajkr8287@users.noreply.github.com>
2024-03-27 09:50:05 +0000

    add dataypes

--- Basic/02_dataTypes.js
+++ Basic/02_dataTypes.js
@@ -19,8 +19,45 @@ let state=null
 // console.log(typeof age);
 
 
-console.log(typeof null);//object
-console.log(typeof undefined);//undefined
+// console.log(typeof null);//object
+// console.log(typeof undefined);//undefined
+
+// JavaScript is a dynamically typed language. Because data type will automatically assigned at the time of compilation or code execution.
+
+// dataTypes are two types
+/* 1:primitive(call by value)
+    7types:String,Number,Boolean,null,undefined,Symbol,BigInt
+
+*/   
+const id=Symbol('123');
+const anotherId=Symbol('123');
+// console.log(id===anotherId);//false
+
+const bigNumber=565468464644648n;
+
+
+/*
+    2:Non primitive (call by reference)
+    ex=>Array,Objects,Functions
+
+*/
+
+const heros=["bheem","motu","shaktiman"];
+console.log(typeof heros);//object
+
+let myObj={
+    name:"dheeraj",
+    age:32,
+
+};
+console.log(typeof myObj);//object
+
+const myFunction=function(){
+    console.log("hello world");
+}
+console.log(typeof myFunction);//function
+
+
 
 
  
