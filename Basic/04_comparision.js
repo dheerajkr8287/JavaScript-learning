4168470e08e02ff6dd33b5a890f82488d510b80d
Dheeraj kumar <119065267+dheerajkr8287@users.noreply.github.com>
2024-03-27 10:42:28 +0000

    comparision&stack/heapMemory

--- Basic/04_comparision.js
+++ Basic/04_comparision.js
@@ -16,4 +16,37 @@
 // console.log(undefined<0);//false
 
 // ===
-console.log("2"===2);//false
+// console.log("2"===2);//false
+
+
+
+//  ********Stack and Heap memory********
+
+// stack (primitive)=>call by value
+// Heap  (non-primitive)=>call by reference
+
+//call by value
+let myname="dheeraj";
+let anotherName=myname;
+anotherName="Neeraj";
+// console.log(myname);   //dheeraj
+// console.log(anotherName); //Neeraj
+
+
+
+
+// call by reference
+let userone={
+    email:"user@gMail.com",
+    upi:"use@ybl",
+};
+let usertwo=userone;
+usertwo.email="dheeraj@gamil.com";
+console.log(userone.email);//dheeraj@gamil.com
+console.log(usertwo.email);//dheeraj@gamil.com
+
+
+
+
+
+
